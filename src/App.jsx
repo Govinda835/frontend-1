920658b723efdec7876b8cdbe7c8519ead807834
Govinda Chaudhary <chaudharygovinda835@gmail.com>
2025-07-16 06:35:22 +0545

    create page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react"
 import {BrowserRouter,Routes,Route} from "react-router-dom"
 import Home from "./Home"
+import Page from "./page"
 
 const App = () => {
   return(
   <BrowserRouter>
      <Routes>
       <Route path="/Home" element={<Home/>}/>
+      <Route path="/page" element={<Page/>}/>
      </Routes>
   </BrowserRouter>
   )
---
+++ src/page.jsx
@@ -0,0 +1,12 @@
+function Page()
+{
+    return(
+        <>
+          <div className="bg-black text-white">
+            <h1>This is the first page of the project</h1>
+          </div>
+        </>
+    )
+}
+
+export default Page
